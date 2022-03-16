8c84f7e267b7a2482158966636d54871ef0ae5ba
Eliud Rodríguez <eliud.giroma@gmail.com>
2022-03-16 15:20:09 -0700

    first commit

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM ubuntu
+
+LABEL Eliud Rodríguez
+
+WORKDIR /root
+
+RUN  apt-get -y update && \
+     apt-get install -yq curl unzip git pip python3.8 python3-pip
+
+RUN pip install csvkit pandas matplotlib kaleido
+
+RUN  curl -L -O https://datosabiertos.salud.gob.mx/gobmx/salud/datos_abiertos/historicos/2022/01/datos_abiertos_covid19_01.01.2022.zip &&\
+     unzip datos_abiertos_covid19_01.01.2022.zip &&\
+     csvcut -c SEXO,EDAD,DIABETES 220101COVID19MEXICO.csv > df_covid19.csv &&\
+     rm datos_abiertos_covid19_01.01.2022.zip &&\
+     rm 220101COVID19MEXICO.csv
+
+ADD codigo.py /root/codigo.py
+
+RUN python3 codigo.py
+
+CMD ["bash"
---
+++ codigo.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("./df_covid19.csv")
+
+df_mujer = df[df.SEXO == 1]
+df_hombre = df[df.SEXO == 2]
+
+
+# HOMBRES
+si_tiene_hombre = list(df_hombre.DIABETES).count(1)
+no_tiene_hombre = list(df_hombre.DIABETES).count(2)
+no_aplica_hombre = list(df_hombre.DIABETES).count(98)
+
+fig = plt.figure()
+ax = fig.add_axes([0,0,1,1])
+etiquetas_hombre = ['Si tiene (hombre)', 'No tiene (hombre)', 'No aplica (hombre)']
+hombres = [si_tiene_hombre,no_tiene_hombre,no_aplica_hombre]
+ax.bar(etiquetas_hombre,hombres)
+plt.show()
+
+
+# MUJERES
+si_tiene_mujer = list(df_mujer.DIABETES).count(1)
+no_tiene_mujer = list(df_mujer.DIABETES).count(2)
+no_aplica_mujer = list(df_mujer.DIABETES).count(98)
+
+fig = plt.figure()
+ax = fig.add_axes([0,0,1,1])
+etiquetas_mujer = ['Si tiene (mujer)', 'No tiene (mujer)', 'No aplica (mujer)']
+mujeres = [si_tiene_mujer,no_tiene_mujer,no_aplica_mujer]
+ax.bar(etiquetas_mujer,mujeres)
+plt.show()
